9cc416ca22d7a3b760824468ee825f55b2539667
Benjamin Froehlich <bdfroehlich0@gmail.com>
2022-02-23 20:52:42 -0600

    adding customazible titles

--- views/layouts/default.jsx
+++ views/layouts/default.jsx
@@ -4,7 +4,9 @@ function Default(html) {
   return (
     <html>
     <head>
-      <title>Default</title>
+        {/* if title is not null display the title if null display default, see breads_controller.js render object title */}
+        {/* need to also add title to the Index function in index.js and as an attribute on the default tag in the Index function */}
+      <title>{html.title || 'Default'}</title>
     </head>
     <body>
       <h1>HTML Rendered!</h1>
